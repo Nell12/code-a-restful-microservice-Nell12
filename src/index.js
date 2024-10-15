f9d685a48bbbd0c975bb97fbef916261986e030e
Aileen Ni <aileenni2003@gmail.com>
2024-10-14 23:00:01 -0400

    Completed REST API microservice

--- src/index.js
+++ src/index.js
@@ -5,12 +5,86 @@ const port = 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+//npm start
+//npm install
 // **************************************************************
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
+let users= [];   //In-memory array
+let uniqueID= 1; //simple autogenerate ID
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+//store data in memory using an array
+//Invoke-RestMethod -Method POST -Uri "<url>" -ContentType 'application/json' -Body '{"content": "Whatever you want to put in here"}' -UseBasicParsing
+app.post( '/users', (req, res) => {
+    //resquest body should contain user's name and email
+    const { name, email } = req.body;
+    if (!name || !email){
+        res.status(404).json({ error: 'Need name and email' });
+    }
+
+    //create new user    
+    const newUser= {
+        id : uniqueID++,
+        "name" : name,
+        "email" : email
+    };          
+
+    users.push(newUser);
+
+    //response is the created user with a unique id
+    res.json(newUser);
+});
+
+//curl http://localhost:3000/users/1
+app.get('/users/:id', (req, res) => {
+    //the user's id will be passed as the URL parameter
+    const id= parseInt(req.params.id);
+
+    //Response is the user's details
+    //Find the ID
+    const user= users.find((item => item.id === id)); 
+
+    if (!user){
+        res.status(404).json({ error: 'User not found' });
+    }
+    else{
+        res.json(user);
+    }
+});
+
+//Invoke-RestMethod -Method PUT -Uri "http://localhost:3000/users/1" -ContentType 'application/json' -Body '{"name": "I", "email": "I@mymail.com"}' -UseBasicParsing
+app.put('/users/:id', (req, res) => {
+    // new name and email for the user
+    const {name, email} = req.body;
+    const id= parseInt(req.params.id);
+
+    // Response is the updated user's details
+    const user= users.findIndex((item => item.id === id));
+
+    if (!user){
+        res.status(404).json({error: "User not found"});
+    }
+    else{
+        users[user]= {id: id, ...{name, email}};
+
+        res.json(users[user]);
+    }
+});
+
+//curl -Method DELETE http://localhost:3000/users/1
+app.delete('/users/:id', (req, res) => {
+    //delete user by id
+    const id= parseInt(req.params.id);
+
+    // Response: 204 no content
+    const user= users.findIndex((item => item.id === id));
+    if(!user){
+        res.status(404).json({error: "User not found"});
+    }
+    else{
+        users.splice(user, 1);
+        res.status(204).send();
+    }
 });
 
 // Do not touch the code below this comment
